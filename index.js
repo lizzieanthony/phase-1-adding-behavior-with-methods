b069e36b65d7186dd46f087577040919f448fc80
Lizzie Anthony <lizziewrab@me.com>
2021-12-02 15:32:00 -0600

    commit tree ae49bf9aa003ff6e6cab13e9f401ab68a3ca9af2

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+class Cat {
+    constructor (name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`
+    }
+  }
+  
+  class Dog {
+    constructor (name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+  }
+  
+  class Bird {
+    constructor (name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        if (this.sex == 'male') {
+            return `It\'s me! ${this.name}, the parrot!`;  
+        } 
+        else {
+            return `${this.name} says squawk!`; 
+        }
+    }
+
+
+
+
+    // if (this.sex == "male") {
+    //     speak() 
+    //     return `It\'s me! ${this.name}, the parrot!`;
+    // }
+    //     else { 
+    //         speak() 
+    //             return `Squawk!`;
+    //     }
+  }
